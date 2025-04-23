dd3cbca92407620abbdc350b79b058d13e555697
ranandasatria <rananda24@gmail.com>
2025-04-23 20:42:45 +0700

    Add files via upload

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+import js from "@eslint/js";
+import { defineConfig } from "eslint/config";
+
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
+  { languageOptions: {
+    globals: {
+      console: "readonly",
+      process: "readonly",
+      setTimeout: "readonly",
+      clearTimeout: "readonly"
+    },
+  }},
+  { rules: {
+    "semi": ["error", "always"],
+    "indent": ["error", 2],
+    "eqeqeq": ["warn", "always"],
+    "comma-spacing": ["warn", { after: true }],
+    "no-redeclare": "error",
+    "no-unused-vars": "off",
+  }}
+]);
